f14ad711bc1ec31af7db53c8aed1f5156463eeb4
ryzzo <ryzzoempire@gmail.com>
2023-02-06 22:21:51 +0300

    project progress

--- league_table/models.py
+++ league_table/models.py
@@ -7,18 +7,21 @@ class Club(models.Model):
     def __str__(self):
         return self.club_name
 
+    class Meta:
+        ordering = ['club_name']
+
 
 class ClubStat(models.Model):
-    club = models.ForeignKey(Club, on_delete=models.CASCADE, db_constraint=False)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
     played = models.PositiveIntegerField(default=0)
     won = models.PositiveIntegerField(default=0)
     draw = models.PositiveIntegerField(default=0)
     losses = models.PositiveIntegerField(default=0)
-    goal_dif = models.PositiveIntegerField(default=0)
+    goal_dif = models.IntegerField(default=0)
     points = models.PositiveIntegerField(default=0)
 
     class Meta:
-        ordering = ['-points']
+        ordering = ('-points','-goal_dif','club',)
 
     def __str__(self):
         return f"{self.club} : {self.points} points"
@@ -29,7 +32,6 @@ class MatchStat(models.Model):
     away_team = models.ForeignKey(Club, on_delete=models.RESTRICT, null=False, related_name='away_team')
     home_goals = models.PositiveIntegerField(default=0)
     away_goals = models.PositiveIntegerField(default=0)
-    winner = models.CharField(max_length=100, blank=False)
 
     def __str__(self):
         return f" {self.home_goals}{self.home_team} - {self.away_team}{self.away_goals}"
--- league_table/views.py
+++ league_table/views.py
@@ -15,12 +15,74 @@ def enterStat(request):
 
             stats_form.save()
             messages.success(request, ('Successfully added'))
+            # retrive data from MatchStat model
+            matchStat_obj = MatchStat.objects.last()
+            home_team = getattr(matchStat_obj, 'home_team')
+            away_team = getattr(matchStat_obj, 'away_team')
+            home_goals = getattr(matchStat_obj, 'home_goals')
+            away_goals = getattr(matchStat_obj, 'away_goals')
+
+            # get ClubStat objects of the clubs
+            home_team_stats = ClubStat.objects.get(club=home_team)
+            away_team_stats = ClubStat.objects.get(club=away_team)
+
+            # home team stats
+            h_played = home_team_stats.played
+            h_won = home_team_stats.won
+            h_draw = home_team_stats.draw
+            h_loss = home_team_stats.losses
+            h_gd = home_team_stats.goal_dif
+            h_points = home_team_stats.points
+
+            # away team stats
+            a_played = away_team_stats.played
+            a_won = away_team_stats.won
+            a_draw = away_team_stats.draw
+            a_loss = away_team_stats.losses
+            a_gd = away_team_stats.goal_dif
+            a_points = away_team_stats.points
+
+            home_team_stats.played = h_played + 1
+            away_team_stats.played = a_played + 1
+            match_goal_diff = abs(home_goals - away_goals)
+            if home_goals > away_goals:
+                home_team_stats.won = h_won + 1
+                away_team_stats.losses = a_loss + 1
+                home_team_stats.goal_dif = h_gd + match_goal_diff
+                away_team_stats.goal_dif = a_gd - match_goal_diff
+                home_team_stats.points = h_points + 3
+                home_team_stats.save()
+                away_team_stats.save()
+            
+            elif home_goals < away_goals:
+                away_team_stats.won = a_won + 1
+                home_team_stats.losses = h_loss + 1
+                away_team_stats.goal_dif = a_gd + match_goal_diff
+                home_team_stats.goal_dif = h_gd - match_goal_diff
+                away_team_stats.points = a_points + 3
+                home_team_stats.save()
+                away_team_stats.save()
+
+            else:
+                home_team_stats.draw = h_draw + 1
+                away_team_stats.draw = a_draw + 1
+                home_team_stats.points = h_points + 1
+                away_team_stats.points = a_points + 1
+                home_team_stats.save()
+                away_team_stats.save()
+                
+
+
         else:
             messages.error(request, 'Error saving form')
 
+       
+
+
+
         return redirect('league_table:enter_stats')
 
-        # add data to the other models
+    
     
     stats_form = MatchStatForm()
 
